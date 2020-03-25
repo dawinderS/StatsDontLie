185d7b1773b7d84a604ef28239187398bfb78539
Dawinder Singh <dawindernsingh@gmail.com>
2020-03-24 17:38:34 -0700

    finish basic setup and navbar

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "main.js"
+  },
+  module: {
+    rules: [
+      {
+        loader: "babel-loader",
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        options: {
+          presets: ["@babel/preset-env"]
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "public")
+  },
+  watch: true
+};
