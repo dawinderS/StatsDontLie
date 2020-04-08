0a70c538fd4ea66a6559254677fb8b3cee291578
Dawinder Singh <dawindernsingh@gmail.com>
2020-04-07 17:00:00 -0700

    add stlying to boxes

--- src/stat_bar.js
+++ src/stat_bar.js
@@ -8,7 +8,7 @@ export const stat_bar = () => {
   let svg2 = d3.select("#stat_bar").append("svg")
     .attr("width", 1000)
     // .attr("min-height", height)
-    .attr("style", "border: thin white solid");
+    // .attr("style", "border: thin white solid");
 
   const top25pts = stats.sort((a, b) => b["22"] - a["22"]).slice(0, stats.length / 2 - 10);
   
@@ -91,47 +91,49 @@ export const stat_bar = () => {
         .attr('class', 'detail')
         .attr('width', '100%')
         .attr('height', 50)
-        .attr('style', 'border: thin #616161 solid')
+        // .attr('style', 'border-top: thin #616161 solid')
 
       const statRect = d3.select("#details")
         .append('rect')
         .attr('class', 'detail2')
         .attr('width', '100%')
         .attr('height', 50)
-        .attr('style', 'border: thin #616161 solid')
+        // .attr('style', 'border: thin #616161 solid')
       const statRect2 = d3.select("#details")
         .append('rect')
-        .attr('class', 'detail2')
+        .attr('class', 'detail3')
         .attr('width', '100%')
         .attr('height', 50)
-        .attr('style', 'border: thin #616161 solid')
+        // .attr('style', 'border: thin #616161 solid')
       
       nameRect
         .append('text')
         .attr('class', 'player_details')
         .style('font-size', '28px')
         .text(d => rTopPts[i]['2'])
-        .style('fill', d => rTopPts[i]['color'])
+        .style('color', d => rTopPts[i]['color'])
       nameRect
         .append('text')
         .attr('class', 'player_details2')
         .style('font-size', '18px')
         .text(d => 'Team: ' + rTopPts[i]['3'])
-        .style('fill', d => rTopPts[i]['color'])
+        .style('color', d => rTopPts[i]['color'])
 
       statRect
         .append('text')
         .attr('class', 'player_details3')
-        .style('font-size', '16px')
-        .text('GP  MIN  FGM  FGA  FGPCT  FG3M  FG3A  FG3PCT  FTA  FTPCT  OREB  DREB  REB  AST  STL  BLK  TO  PTS')
+        .style('font-size', '18px')
+        // .text('GP  MIN  FGM  FGA  FGPCT  FG3M  FG3A  FG3PCT  FTA  FTPCT  OREB  DREB  REB  AST  STL  BLK  TO  PTS')
+        .text(d => 'Pts:  ' + rTopPts[i]['22'] + '   |   ' + 'Reb:  ' + rTopPts[i]['17'] + '   |   ' + 'Ast:  ' + rTopPts[i]['18'] + '   |   ' + 'Stl:  ' + rTopPts[i]['19'] + '   |   ' + 'Blk:  ' + rTopPts[i]['20'] + '   |   ' + 'TOV:  ' + rTopPts[i]['21'] + '   |   ' + 'GP:  ' + rTopPts[i]['4'] + '   |   ' + 'Min:  ' + rTopPts[i]['5'])
+
       statRect2
         .append('text')
         .attr('class', 'player_details4')
-        .style('font-size', '18px')
-        .text(d => rTopPts[i]['4'] + '   ' + rTopPts[i]['5'] + '    ' + rTopPts[i]['6'] + '    ' + rTopPts[i]['7'] + '    ' + rTopPts[i]['8'] + '   ' + rTopPts[i]['9'] + ' ' + rTopPts[i]['10'] + ' ' + rTopPts[i]['11'] + ' ' + rTopPts[i]['12'] + ' ' + rTopPts[i]['13'] + ' ' + rTopPts[i]['14'] + ' ' + rTopPts[i]['15'] + ' ' + rTopPts[i]['16'] + ' ' + rTopPts[i]['17'] + ' ' + rTopPts[i]['18'] + ' ' + rTopPts[i]['19'] + ' ' + rTopPts[i]['20'] + ' ' + rTopPts[i]['21'] + ' ' + rTopPts[i]['22'])
-        // .text(d => rTopPts[i]['2'])
+        .style('font-size', '17px')
+        // .text(d => rTopPts[i]['4'] + '   ' + rTopPts[i]['5'] + '    ' + rTopPts[i]['6'] + '      ' + rTopPts[i]['7'] + '    ' + rTopPts[i]['8'] + '    ' + rTopPts[i]['9'] + '    ' + rTopPts[i]['10'] + ' ' + rTopPts[i]['11'] + ' ' + rTopPts[i]['12'] + ' ' + rTopPts[i]['13'] + ' ' + rTopPts[i]['14'] + ' ' + rTopPts[i]['15'] + ' ' + rTopPts[i]['16'] + ' ' + rTopPts[i]['17'] + ' ' + rTopPts[i]['18'] + ' ' + rTopPts[i]['19'] + ' ' + rTopPts[i]['20'] + ' ' + rTopPts[i]['21'] + ' ' + rTopPts[i]['22'])
+        .text(d => 'Advanced:    Eff_Rank:  ' + rTopPts[i]['1'] + '    |    ' + 'Eff:  ' + rTopPts[i]['23'] + '    |    ' + 'Fg_Pct:  ' + rTopPts[i]['8'] + '    |    ' + 'Fg3_Pct:  ' + rTopPts[i]['11'] + '    |    ' + 'Ft_Pct:  ' + rTopPts[i]['14'])
       
-      window.scrollBy(0, 300);
+      window.scrollBy(0, 350);
     });
 
   svg2.selectAll('rect')
