2436a167e683adab1161f18db97667262279b182
Dawinder Singh <dawindernsingh@gmail.com>
2020-04-08 03:23:12 -0700

    add circles to data

--- src/bubble_chart.js
+++ src/bubble_chart.js
@@ -7,27 +7,62 @@ export const bubble_chart = () => {
     .attr('height', 640)
     // .attr('style', 'border: thin white solid')
     // .style('background-color', 'white')
-  
+  let width = 1000;
+  let height = 640;
+
   let pack = d3.pack().size([1000, 600]).padding(3);
   let data = stats.sort((a,b) => b['22'] - a['22']).slice(0, 30);
   let hierarchy1 = d3.hierarchy({ children: data }).sum(d => d['22']);
 
   const root = pack(hierarchy1);
 
+  let simulation = d3.forceSimulation()
+    .force("center", d3.forceCenter().x(width / 2).y(height / 2))
+    .force("collide", d3.forceCollide().strength(.01).radius(30).iterations(1))
+    .force("charge", d3.forceManyBody().strength(0.5))
+
   const leaf = svg.selectAll("g")
     .data(root.leaves())
     .join("g")
     .attr("transform", d => `translate(500,360)`)
-    .on('click', function() {
+    .on('click', function () {
       svg.selectAll('g')
-        .attr('transform', d => `translate(${d.x},${d.y})`)
+        .attr('transform', (d,i) => {
+          let x = 0;
+          let y = 0;
+          if (i < 6 ) {
+            x = (i%6) * 145 + 140;
+            y = 125 * Math.floor(i / 6) + 115;
+          } else if (i < 12) {
+            x = (i % 6) * 135 + 160;
+            y = 125 * Math.floor(i / 6) + 115;
+          } else if (i < 18) {
+            x = (i % 6) * 125 + 180;
+            y = 115 * Math.floor(i / 6) + 115;
+          } else if (i < 24) {
+            x = (i % 6) * 115 + 200;
+            y = 109 * Math.floor(i / 6) + 115;
+          } else if (i < 30) {
+            x = (i % 6) * 105 + 220;
+            y = 105 * Math.floor(i / 6) + 115;
+          }
+          console.log(i)
+          
+          return "translate(" + [x,y] + ")"
+        })
+      simulation.nodes(data)
+        .on('tick', function (d) {
+          leaf
+            .attr('cx', d => d.x)
+            .attr('cy', d => d.y + 1252)
+        })
+      // document.querySelector('.leaves').style.display = 'none'
     })
-    // .attr("transform", d => `translate(0,0)`)
 
   const circles = leaf.append("circle")
     .attr('class', 'stat-circles')
     .attr("r", d => {
-      if (d.data['22'] > 28) {
+      if (d.data['22'] > 29) {
         return d.data['22'] + 25
       } else if (d.data['22'] > 25) {
         return d.data['22'] + 20
@@ -37,26 +72,81 @@ export const bubble_chart = () => {
         return d.data['22'] + 15
       }
       })
-      // d.data['22'] + 18)
-    .attr("fill-opacity", 0.70)
+    .attr("fill-opacity", 0.90)
     .attr("fill", d => 'black')
     .attr('stroke', d => d.data.color)
     .attr('stroke-width', d => 4)
-  
+    .on('click', function (s, i) {
+      const nameRect = d3.select("#details")
+        .append('rect')
+        .attr('class', 'detail')
+        .attr('width', '100%')
+        .attr('height', 50)
+      // .attr('style', 'border-top: thin #616161 solid')
 
+      const statRect = d3.select("#details")
+        .append('rect')
+        .attr('class', 'detail2')
+        .attr('width', '100%')
+        .attr('height', 50)
+      // .attr('style', 'border: thin #616161 solid')
+      const statRect2 = d3.select("#details")
+        .append('rect')
+        .attr('class', 'detail3')
+        .attr('width', '100%')
+        .attr('height', 50)
+      // .attr('style', 'border: thin #616161 solid')
+
+      nameRect
+        .append('text')
+        .attr('class', 'player_details')
+        .style('font-size', '28px')
+        .text(d => data[i]['2'])
+        .style('color', d => data[i]['color'])
+      nameRect
+        .append('text')
+        .attr('class', 'player_details2')
+        .style('font-size', '18px')
+        .text(d => 'Team: ' + data[i]['3'])
+        .style('color', d => data[i]['color'])
+
+      statRect
+        .append('text')
+        .attr('class', 'player_details3')
+        .style('font-size', '18px')
+        // .text('GP  MIN  FGM  FGA  FGPCT  FG3M  FG3A  FG3PCT  FTA  FTPCT  OREB  DREB  REB  AST  STL  BLK  TO  PTS')
+        .text(d => 'Pts:  ' + data[i]['22'] + '   |   ' + 'Reb:  ' + data[i]['17'] + '   |   ' + 'Ast:  ' + data[i]['18'] + '   |   ' + 'Stl:  ' + data[i]['19'] + '   |   ' + 'Blk:  ' + data[i]['20'] + '   |   ' + 'TOV:  ' + data[i]['21'] + '   |   ' + 'GP:  ' + data[i]['4'] + '   |   ' + 'Min:  ' + data[i]['5'])
+
+      statRect2
+        .append('text')
+        .attr('class', 'player_details4')
+        .style('font-size', '17px')
+        // .text(d => data[i]['4'] + '   ' + data[i]['5'] + '    ' + data[i]['6'] + '      ' + data[i]['7'] + '    ' + data[i]['8'] + '    ' + data[i]['9'] + '    ' + data[i]['10'] + ' ' + data[i]['11'] + ' ' + data[i]['12'] + ' ' + data[i]['13'] + ' ' + data[i]['14'] + ' ' + data[i]['15'] + ' ' + data[i]['16'] + ' ' + data[i]['17'] + ' ' + data[i]['18'] + ' ' + data[i]['19'] + ' ' + data[i]['20'] + ' ' + data[i]['21'] + ' ' + data[i]['22'])
+        .text(d => 'Advanced:    Eff_Rank:  ' + data[i]['1'] + '    |    ' + 'Eff:  ' + data[i]['23'] + '    |    ' + 'Fg_Pct:  ' + data[i]['8'] + '    |    ' + 'Fg3_Pct:  ' + data[i]['11'] + '    |    ' + 'Ft_Pct:  ' + data[i]['14'])
+
+      window.scrollBy(0, 350);
+    });
+    
   leaf.append("text")
   .style('fill', 'white')
   .style('text-anchor', 'middle')
   // .style('font-size', '25px')
   .style('font-size', d => d.r / 1.5)
   .text(d => d.data['22'])
+  .attr('dy', '0.25em')
 
   leaf.append("text")
-  .attr('dy', '1.5em')
+  .attr('dy', '1.7em')
   .style('fill', 'white')
   .style('text-anchor', 'middle')
   .style('font-size', d => d.r / 3)
   .text(d => d.data['2'].split(' ').slice(1))
+  leaf.append("text")
+  .attr('dy', '-1.9em')
+  .style('fill', 'white')
+  .style('text-anchor', 'middle')
+  .style('font-size', d => d.r / 3.9)
+  .text(d => d.data['2'].split(' ').slice(0,1))
 
   return svg.node();
 }
