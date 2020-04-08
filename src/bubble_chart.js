a319fe69bf26b8e8e7c7999e7d43384a39b3ecad
Dawinder Singh <dawindernsingh@gmail.com>
2020-04-07 23:59:03 -0700

    fix circles to the court

--- src/bubble_chart.js
+++ src/bubble_chart.js
@@ -4,12 +4,12 @@ export const bubble_chart = () => {
   let svg = d3.select('#top_per').append('svg')
     .attr('class', 'bubble_svg')
     .attr('width', 1000)
-    .attr('height', 700)
+    .attr('height', 640)
     // .attr('style', 'border: thin white solid')
     // .style('background-color', 'white')
   
-  let pack = d3.pack().size([900, 700]).padding(3);
-  let data = stats.sort((a,b) => b['22'] - a['22']);
+  let pack = d3.pack().size([1000, 600]).padding(3);
+  let data = stats.sort((a,b) => b['22'] - a['22']).slice(0, 30);
   let hierarchy1 = d3.hierarchy({ children: data }).sum(d => d['22']);
 
   const root = pack(hierarchy1);
@@ -17,47 +17,46 @@ export const bubble_chart = () => {
   const leaf = svg.selectAll("g")
     .data(root.leaves())
     .join("g")
-    .attr("transform", d => `translate(${d.x + 1},${d.y + 1})`)
+    .attr("transform", d => `translate(500,360)`)
+    .on('click', function() {
+      svg.selectAll('g')
+        .attr('transform', d => `translate(${d.x},${d.y})`)
+    })
     // .attr("transform", d => `translate(0,0)`)
 
-  leaf.append("circle")
+  const circles = leaf.append("circle")
+    .attr('class', 'stat-circles')
     .attr("r", d => {
       if (d.data['22'] > 28) {
-        return d.data['22'] + 30
-      } else if (d.data['22'] > 25) {
         return d.data['22'] + 25
+      } else if (d.data['22'] > 25) {
+        return d.data['22'] + 20
       } else if (d.data['22'] > 20) {
-        return d.data['22'] + 20
+        return d.data['22'] + 15
       } else {
-        return d.data['22'] + 20
+        return d.data['22'] + 15
       }
       })
       // d.data['22'] + 18)
-    .attr("fill-opacity", 0.95)
-    .attr("fill", d => d.data.color)
+    .attr("fill-opacity", 0.70)
+    .attr("fill", d => 'black')
+    .attr('stroke', d => d.data.color)
+    .attr('stroke-width', d => 4)
   
 
   leaf.append("text")
-    .text(d => d.data['22'])
+  .style('fill', 'white')
+  .style('text-anchor', 'middle')
+  // .style('font-size', '25px')
+  .style('font-size', d => d.r / 1.5)
+  .text(d => d.data['22'])
 
-  // let vis = leaf.selectAll("circle")
-  //   .data(nodes, d => d['2'])
-
-  // leaf.append("clipPath")
-  //   .append("use")
-  //   .attr("xlink:href", d => d.leafUid.href);
-
-  // leaf.append("text")
-  //   .attr("clip-path", d => d.clipUid)
-  //   .selectAll("tspan")
-  //   .data(d => d.data.name.split(/(?=[A-Z][a-z])|\s+/g))
-  //   .join("tspan")
-  //   .attr("x", 0)
-  //   .attr("y", (d, i, nodes) => `${i - nodes.length / 2 + 0.8}em`)
-  //   .text(d => d);
-
-  // leaf.append("title")
-  //   .text(d => `${d.data.title === undefined ? "" : `${d.data.title}`}${format(d.value)}`);
+  leaf.append("text")
+  .attr('dy', '1.5em')
+  .style('fill', 'white')
+  .style('text-anchor', 'middle')
+  .style('font-size', d => d.r / 3)
+  .text(d => d.data['2'].split(' ').slice(1))
 
   return svg.node();
 }
